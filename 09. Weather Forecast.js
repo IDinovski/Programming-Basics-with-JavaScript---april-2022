7700198cd2ab9fe06dbd89f771e86e009545c851
IDinovski <idinovski@gmail.com>
2022-08-01 12:29:26 +0300

    Delete 09. Weather Forecast.js

--- 09. Weather Forecast.js
+++
@@ -1,9 +0,0 @@
-function weatherForecast (input){
-    let vreme = String(input[0]);
-if (vreme === `sunny`){
-    console.log(`It's warm outside!`);
-} else{
-    console.log(`It's cold outside!`);
-}
-}
-weatherForecast (["sunny"])
