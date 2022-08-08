ca271bffc6fdd820282991ee35022c5e930d3b51
IDinovski <idinovski@gmail.com>
2022-08-08 07:04:59 +0300

    Create number100-200.js

---
+++ lesson 5-6  Conditional Statements/number100-200.js
@@ -0,0 +1,13 @@
+function number100200(input){
+    let chislo = Number(input[0]);
+
+    if (chislo < 100){
+        console.log("Less than 100")
+     }  else if (200 >= chislo){
+        console.log("Between 100 and 200")
+
+    } else if (chislo > 200)  {
+        console.log("Greater than 200")
+        }
+}
+number100200([1]);
