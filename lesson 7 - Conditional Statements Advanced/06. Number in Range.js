677c3745aaf39fea04f9c51fa2a4817157318616
IDinovski <idinovski@gmail.com>
2022-08-11 12:52:44 +0300

    Create 06. Number in Range.js

---
+++ lesson 7 - Conditional Statements Advanced/06. Number in Range.js
@@ -0,0 +1,10 @@
+function numberInRange(input) {
+    let num = Number(input[0]);
+    if (num >= -100 && num <= 100 && num !== 0 ) {
+        console.log(`Yes`);
+    } else if (num === 0) {
+        console.log(`No`);
+    } else {
+        console.log(`No`);
+    }
+}
