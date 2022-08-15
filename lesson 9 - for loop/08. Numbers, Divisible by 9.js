ddbae6bacfed7520c582e47bc883ae1b8019f002
IDinovski <idinovski@gmail.com>
2022-08-15 12:04:32 +0300

    Create 08. Numbers, Divisible by 9.js

---
+++ lesson 9 - for loop/08. Numbers, Divisible by 9.js
@@ -0,0 +1,16 @@
+function solve(input){
+    let start = Number(input[0]);
+    let end = Number(input[1]);
+    let sum = 0
+    for (let i = start ; i <=end; i++){
+        if(i%9===0){
+            sum+=i;
+        }
+    }
+    console.log(`The sum: ${sum}`);
+    for (let i = start; i <=end; i++){
+        if(i%9===0){
+        console.log(i);
+        }
+    }
+}
