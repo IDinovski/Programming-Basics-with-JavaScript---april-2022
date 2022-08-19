faa733440e772d15776e829c1a2c40ce59571ed2
IDinovski <idinovski@gmail.com>
2022-08-19 10:54:19 +0300

    Create 02. Multiplication Table.js

---
+++ Nested loops - Lab/02. Multiplication Table.js
@@ -0,0 +1,9 @@
+function solve(input){
+for(let pyrvi = 1;pyrvi<=10;pyrvi++){
+    for (let vtori = 1;vtori<=10;vtori++){
+        let result = pyrvi*vtori;
+        console.log(`${pyrvi} * ${vtori} = ${result}`);
+    }
+}
+}
+solve()
